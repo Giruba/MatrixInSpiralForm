b4b877f569adcfe3d1497af183509bcfc8a21374
gibeulah <gibeulah@microsoft.com>
2018-11-05 15:44:41 +0530

    First commit

---
+++ MatrixInSpiralForm/MatrixProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixInSpiralForm
+{
+    class MatrixProcessor
+    {
+        int[,] matrix;
+
+        public MatrixProcessor(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void PrintInSpiralForm() {
+
+            Console.WriteLine("*******The input matrix*******");
+            PrintMatrix(matrix);
+
+            int m = 0, n = 0;
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            Console.WriteLine("---Printing the matrix in spiral form---");
+            while (m < rows && n < columns) {
+                for (int i = n; i < columns; i++) {
+                    Console.Write(matrix[m,i]+" ");
+                }
+                //Increment m as one row has been processed
+                m++;
+
+                for (int i = m; i < rows; i++) {
+                    Console.Write(matrix[i,columns-1]+" ");
+                }
+                //Decrement as the last column has been processed
+                columns--;
+
+                if (m < rows) {
+                    for (int i = columns - 1; i >= n; i--) {
+                        Console.Write(matrix[rows-1,i]+" ");
+                    }
+                    //Decrement rows as the last rows has been processed
+                    rows--;
+                }
+
+                if (n < columns) {
+                    for (int i = rows - 1; i >= m; i--) {
+                        Console.Write(matrix[i,n]+" ");
+                    }
+                    n++;
+                    //Increment n as first column is processed
+                }
+            }
+        }
+
+        private void PrintMatrix(int[,] matrix) {
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < matrix.GetLength(0); i++) {
+                for (int j = 0; j < matrix.GetLength(1); j++) {
+                    Console.Write(matrix[i,j]+ " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--------------------");
+        }
+
+    }
+}
--- MatrixInSpiralForm/Program.cs
+++ MatrixInSpiralForm/Program.cs
@@ -6,7 +6,45 @@ namespace MatrixInSpiralForm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Matrix in Spiral Form!");
+            Console.WriteLine("----------------------");
+
+            try
+            {
+                int[,] matrix = GetInput();
+                MatrixProcessor matrixProcessor = new MatrixProcessor(matrix);
+                matrixProcessor.PrintInSpiralForm();
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is"+exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int[,] GetInput() {
+
+            int[,] matrix = null;
+            Console.WriteLine("Enter the number of rows in the matrix");
+            try
+            {
+                int rows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of columns in the matrix");
+                int columns = int.Parse(Console.ReadLine());
+                matrix = new int[rows,columns];
+                for (int i = 0; i < rows; i++) {
+                    Console.WriteLine("Enter the elements of row " + i + " separated by space");
+                    String rowElements = Console.ReadLine();
+                    String[] elements = rowElements.Split(' ');
+                    for (int j = 0; j < columns; j++) {
+                        matrix[i, j] = int.Parse(elements[j]);
+                    }
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is"+exception.Message);
+            }
+            return matrix;
         }
     }
 }
